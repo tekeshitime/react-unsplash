2e0ea0ca1f490426ccee1356576a0590858bb44f
tekeshitime <kyaputenn13@gmail.com>
2024-02-21 16:20:08 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Header from "./components/Header";
+import "./App.css";
+import Search from "./components/Search";
+import unsplash from "./api/unsplash";
+import ImageList from "./components/ImageList";
+
+function App() {
+  const [images, setImages] = React.useState([]);
+  const onSearchSubmit = async (term) => {
+    const response = await unsplash.get("/search/photos", {
+      params: { query: term },
+    });
+    setImages(response.data.result);
+  };
+
+  return (
+    <div className="App">
+      <Header>
+        <Search onSearchSubmit={onSearchSubmit} />
+      </Header>
+      <div>
+        <ImageList images={images} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import "./Header.css";
+
+const Header = ({ children }) => {
+  return (
+    <>
+      <div className="Header">
+        <span className="Header__Logo">TypeScrash</span>
+        {children}
+      </div>
+    </>
+  );
+};
+export default Header;
